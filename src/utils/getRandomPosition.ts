f2863124b0fdf40903552692a677b9bad4c1f10e
A.Daneshpour <amirhosseindaneshpour@gmail.com>
2024-07-25 11:33:40 +0330

    remove hard codes

--- src/utils/getRandomPosition.ts
+++ src/utils/getRandomPosition.ts
@@ -1,6 +1,7 @@
+import { BOARD_HEIGHT, BOARD_WIDTH } from "../constants/constants";
 import { Coordinate } from "../types/types";
 
 export const getRandomPosition = (): Coordinate => ({
-    x: Math.floor(Math.random() * 25),
-    y: Math.floor(Math.random() * 18),
+    x: Math.floor(Math.random() * BOARD_WIDTH),
+    y: Math.floor(Math.random() * BOARD_HEIGHT),
   });
